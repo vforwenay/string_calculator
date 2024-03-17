f9e9fb501daafdbe042c0fdd5e2920dbead332cf
Vinay Rathore <vinayharda244@gmail.com>
2024-03-17 21:44:50 +0530

    Add test casehandle multiple delimiters with length longer than one char

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -56,5 +56,10 @@ RSpec.describe StringCalculator do
       calculator = StringCalculator.new
       expect(calculator.add("//[;][*]\n1;2*3")).to eq(6)
     end
+
+    it 'supports multiple delimiters with length longer than one character' do
+      calculator = StringCalculator.new
+      expect(calculator.add("//[***][%%%]\n1***2%%%3")).to eq(6)
+    end
   end
 end
