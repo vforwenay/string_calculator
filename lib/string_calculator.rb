3503f52b2f28638c7c6dade08d7635b31d81f371
Vinay Rathore <vinayharda244@gmail.com>
2024-03-17 21:31:05 +0530

    Refactor and optimize logic

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,18 +2,34 @@ class StringCalculator
   def add(numbers)
     return 0 unless numbers.is_a?(String)
 
+    delimiter, numbers = extract_delimiter(numbers)
+
+    numbers_array = parse_numbers(numbers, delimiter)
+
+    check_negatives(numbers_array)
+
+    numbers_array.reject! { |num| num > 1000 }
+    numbers_array.sum
+  end
+
+  private
+
+  def extract_delimiter(numbers)
     delimiter = ','
     if numbers.start_with?("//")
       delimiter_end = numbers.index("\n")
       delimiter = numbers[2...delimiter_end]
       numbers = numbers[(delimiter_end + 1)..-1]
     end
+    [delimiter, numbers]
+  end
 
-    numbers_array = numbers.split(/[,#{Regexp.escape(delimiter)}\n]/).map(&:to_i)
+  def parse_numbers(numbers, delimiter)
+    numbers.split(/[,#{Regexp.escape(delimiter)}\n]/).map(&:to_i)
+  end
+
+  def check_negatives(numbers_array)
     negatives = numbers_array.select { |num| num < 0 }
     raise "negatives not allowed: #{negatives.join(', ')}" unless negatives.empty?
-
-    numbers_array.reject! { |num| num > 1000 }
-    numbers_array.sum
   end
 end
