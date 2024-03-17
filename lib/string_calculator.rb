d4f84051251737deba75066b4090e409c0d417f7
Vinay Rathore <vinayharda244@gmail.com>
2024-03-17 21:13:37 +0530

    Implement logic for negative numbers and add error msg

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,12 +3,15 @@ class StringCalculator
     return 0 unless numbers.is_a?(String)
 
     delimiter = ','
-
     if numbers.start_with?("//")
       delimiter = numbers[2]
       numbers = numbers[4..-1]
     end
 
-    numbers.split(/[,#{delimiter}\n]/).map(&:to_i).sum
+    numbers_array = numbers.split(/[,#{delimiter}\n]/).map(&:to_i)
+    negatives = numbers_array.select { |num| num < 0 }
+    raise "negatives not allowed: #{negatives.join(', ')}" unless negatives.empty?
+
+    numbers_array.sum
   end
 end
