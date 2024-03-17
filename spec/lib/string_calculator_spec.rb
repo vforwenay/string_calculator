90cf529fb8fc312e496498d7312a1ec02e5f0b3c
Vinay Rathore <vinayharda244@gmail.com>
2024-03-17 20:38:30 +0530

    Add failing tests for one numbers

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe StringCalculator do
       calculator = StringCalculator.new
       expect(calculator.add('')).to eq(0)
     end
+
+    it 'returns the number for a single number' do
+      calculator = StringCalculator.new
+      expect(calculator.add('1')).to eq(1)
+    end
   end
 end
